1514cd483e76ef9cf767761423a590979a8d8714
Denys Yakovenko <dd.yakovenko@gmail.com>
2017-06-30 18:52:31 +0300

    Moves Row to the SubViews folder

--- js/App/Components/Schedule/Action.js
+++ js/App/Components/Schedule/Action.js
@@ -24,7 +24,7 @@
 import React, { PropTypes } from 'react';
 import { Dimensions } from 'react-native';
 import { View, ListDataSource, List, Text } from 'BaseComponents';
-import Row from './Row';
+import Row from './SubViews/Row';
 import Theme from 'Theme';
 
 const actions = [
