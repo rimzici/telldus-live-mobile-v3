ba1e560fd546d644829e4d93f635b0a861efb329
tks.asahina <tks.asahina@gmail.com>
2017-08-16 22:44:37 +0200

    added native android packages to the application

--- android/app/src/main/java/com/telldus/live/mobile/MainApplication.java
+++ android/app/src/main/java/com/telldus/live/mobile/MainApplication.java
@@ -34,9 +34,13 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
+import com.smixx.fabric.FabricPackage;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
 import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
+import com.benwixen.rnfilesystem.RNFileSystemPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -53,7 +57,8 @@ public class MainApplication extends Application implements ReactApplication {
 				new VectorIconsPackage(),
 				new ExtraDimensionsPackage(),
 				new MainReactPackage(),
-				new ReactNativePushNotificationPackage()
+				new ReactNativePushNotificationPackage(),
+				new RNFileSystemPackage()
 			);
 		}
 	};
@@ -66,6 +71,12 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        // Set up Crashlytics, disabled for debug builds
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+          .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+          .build();
+        Fabric.with(this, crashlyticsKit);
         SoLoader.init(this, /* native exopackage */ false);
-    }
+//    	AndroidNetworking.initialize(getApplicationContext());
+	}
 }
